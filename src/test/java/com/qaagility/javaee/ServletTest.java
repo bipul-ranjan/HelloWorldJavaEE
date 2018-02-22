e5b145f0cfaca17c7b92eb409cab8ed1da279370
Bipul Ranjan <bipulranjankumar@gmail.com>
2018-02-23 01:17:22 +0530

    Remove cal files

---
+++ src/test/java/com/qaagility/javaee/ServletTest.java
@@ -0,0 +1,23 @@
+package test.java.com.qaagility.javaee;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import main.java.com.qaagility.javaee.*;
+ 
+public class ServletTest {
+  //HelloServlet hello = new HelloServlet();
+
+      public void testSlow() {
+        System.out.println("slow");
+      }
+
+      public void testSlower() {
+        System.out.println("slower");
+      }
+
+      public void testFast() {
+        System.out.println("fast-updated");
+      }
+
+	
+}
